a0dd3872bd39898d81fbadc3a748e8daba88bbd8
David <davzeller@gmail.com>
2021-03-03 11:44:58 +0100

    testing

--- AuroraServices/Android.mk
+++ AuroraServices/Android.mk
@@ -6,7 +6,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := permissions_com.aurora.services.xml
+LOCAL_MODULE := privapp-permissions-auroraservices.xml
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
@@ -21,6 +21,5 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_REQUIRED_MODULES := permissions_com.aurora.services.xml
+LOCAL_REQUIRED_MODULES := privapp-permissions-auroraservices.xml
 include $(BUILD_PREBUILT)
-
